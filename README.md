e63b822c0e572baa4ebac4689e4bdd6b01eb52f8
Atakito9 <atakitoback@gmail.com>
2026-03-13 10:38:11 +0100

    Test innit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Sd_P2
+
+![](https://img.shields.io/badge/version-test-red)
+![](https://upload.wikimedia.org/wikipedia/commons/e/ea/Netflix_Logomark.png)
+## Descripcion de la practica
---
+++ Servidor/client.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const server = 'http://localhost:8080'
+
+async function testHelloWorld(){
+    const result = await axios.get(server + '/')
+    return result.data // el campo data contendrá el resultado
+}
+
+const hello = await testHelloWorld()
+console.log('Prueba de conexión, resultado: ' + hello)
