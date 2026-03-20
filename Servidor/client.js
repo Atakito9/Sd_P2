8081d7004cfb03183365bead2fd7a9c386e6c455
Skyckino <daniel.garciamarquez@estudiante.uam.es>
2026-03-20 09:43:00 +0100

    test3

--- Servidor/client.js
+++ Servidor/client.js
@@ -1,11 +1,20 @@
 import axios from 'axios'
-
+// Dirección base del servidor
 const server = 'http://localhost:8080'
-
-async function testHelloWorld(){
-    const result = await axios.get(server + '/')
-    return result.data // el campo data contendrá el resultado
-}
-
-const hello = await testHelloWorld()
-console.log('Prueba de conexión, resultado: ' + hello)
+// Función main
+async function main() {
+    try {
+// Probamos la conexión básica
+    console.log('--- Prueba de conexión ---')
+    const hello = await axios.get(server + '/')
+    console.log(hello.data)
+// Creamos varias películas
+        console.log('\n--- Crear películas ---')
+        console.log((await axios.put(server + '/movie/m1', {
+            title: 'Inception',
+            genre: 'sci-fi',
+            year: 2010,
+            in_cinema: false
+        })).data)
+    }catch{}
+} 
